de1e7ff8e7b032191fa33cb25f6ccc2fec4c7d20
ivancww <wahchiu0903@gmail.com>
2026-06-11 17:31:10 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,22 @@
-// ====== 每次更新 HTML，請務必更改這個版本號 ======
-const CACHE_NAME = 'ava-system-v3.8.1'; 
-// ================================================
-
+const CACHE_NAME = 'ava-standalone-v4.1.0';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo-192.png',
-  './logo-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo-192.png'
 ];
 
 self.addEventListener('install', event => {
-  // 強制要求新的 Service Worker 立即接管，不等待舊版關閉
-  self.skipWaiting(); 
-  
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
 
@@ -32,11 +27,10 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // 刪除所有舊版本的 Cache
             return caches.delete(cacheName);
           }
         })
       );
-    }).then(() => self.clients.claim()) // 強制立即控制所有打開的頁面
+    })
   );
 });
